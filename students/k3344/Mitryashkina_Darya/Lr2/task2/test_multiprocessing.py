e1db8bfc6ff1f16d31e76590ffeb1260012594b5
Darya <104630608+daryaami@users.noreply.github.com>
2025-05-07 18:07:56 +0300

    add task 2 lab 2

--- students/k3344/Mitryashkina_Darya/Lr2/task2/test_multiprocessing.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-import time
-
-if __name__ == "__main__":
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    print(f"Time taken: {end_time - start_time:.2f} seconds")
